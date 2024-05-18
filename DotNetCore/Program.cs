7e4f3f3e68eb029279aceb5fe8aee139c9da1a5b
Chirag980 <chiragsahu980@gmail.com>
2024-05-18 22:33:02 +0530

    Setup_ApplicationDBContext

--- DotNetCore/Program.cs
+++ DotNetCore/Program.cs
@@ -1,7 +1,12 @@
+using DotNetCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Hey we want to use Entity Framework core
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
