5dbbed76993110393698664f03f156e175d78158
Chirag980 <chiragsahu980@gmail.com>
2024-05-18 19:08:39 +0530

    Started DotNetCore and Implemented the connection string to connect with the ssms server

---
+++ DotNetCore/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryType { get; set; }
+    }
+}
