2358801a45935e65d7ff09245ac8b684402ba5b7
Chirag980 <chiragsahu980@gmail.com>
2024-05-19 15:58:48 +0530

    InsertDataInCategoryTable

--- DotNetCore/Data/ApplicationDbContext.cs
+++ DotNetCore/Data/ApplicationDbContext.cs
@@ -11,5 +11,12 @@ namespace DotNetCore.Data
         }
         //From here we are creating table thats the beauty of the entity framework
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Action",DisplayOrder=1},
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 } );
+        }
     }
 }
