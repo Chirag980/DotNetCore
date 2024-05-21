3cfe46e2786d648e3d540e663f0a2f2978d92823
Chirag980 <chiragsahu980@gmail.com>
2024-05-21 07:17:23 +0530

    Added Bootstrap for better css

--- DotNetCore/Controllers/CategoryController.cs
+++ DotNetCore/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCore.Data;
+using DotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();//This code will run like Select * from Category Table it is for getting the all the data from the parrticular model or table
+            return View(objCategoryList);
         }
     }
 }
